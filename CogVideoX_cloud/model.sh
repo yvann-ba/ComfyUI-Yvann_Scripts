d046a0fa6442da8c36098387cdf037656a742761
Yvann <barbot.yvann@gmail.com>
2024-12-20 10:52:26 +0100

    remove model since its  on launch

--- CogVideoX_cloud/model.sh
+++
@@ -1,9 +0,0 @@
-cd /workspace/ComfyUI/models/
-
-cd CogVideo/loras
-wget -O CogVideoX-Fun-V1.1-5b-InP-MPS.safetensors https://huggingface.co/alibaba-pai/CogVideoX-Fun-V1.1-Reward-LoRAs/resolve/main/CogVideoX-Fun-V1.1-5b-InP-MPS.safetensors?download=true
-
-cd ../clip
-wget -O t5xxl_fp8_e4m3fn.safetensors https://huggingface.co/mcmonkey/google_t5-v1_1-xxl_encoderonly/resolve/main/t5xxl_fp8_e4m3fn.safetensors?download=true
-
-cd ../..
